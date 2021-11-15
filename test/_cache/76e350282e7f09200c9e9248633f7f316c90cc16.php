176faac4382ed854debf42461064145496e40d70
Guilherme de Alacoc Aquino <guilhermealacoc@gmail.com>
2021-11-14 23:36:42 -0300

    apagados

--- test/_cache/76e350282e7f09200c9e9248633f7f316c90cc16.php
+++
@@ -1,24 +0,0 @@
-            <?php $__env->startSection('view::extends', '_includes.base'); ?>
-<?php $__env->startSection('view::yields', 'body'); ?>
-<?php $__env->startSection('pageTitle', '- Some Markdown'); ?>
-
-            <?php $__env->startSection('body'); ?>
-            <h1><?php echo e($siteName); ?></h1>
-<p>While this is a pure <code>.md</code> <strong>file</strong>, you can still call variables the Blade way.</p>
-<h4>The following is a product of a Blade foreach loop working with a pure markdown syntax.</h4>
-<p><?php foreach(['Mon','Tues','Wednes'] as $word): ?>
-<?php echo e($word); ?><strong>day</strong>
-<?php endforeach; ?></p>
-<div style="color:blue;">
-This markdown won't **work**.
-<div>
-
-<div style="color:blue;">
-<?php echo \Katana\Markdown::parse(<<<'EOT'
-But this will **work**.
-
-EOT
-); ?>
-<div>
-            <?php $__env->stopSection(); ?>
-<?php echo $__env->make('_includes.base', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
--- test/_cache/e99af4e134152bb7e3f7810223677ada663b1c67.php
+++
@@ -1,15 +0,0 @@
-            <?php $__env->startSection('view::extends', '_includes.blog_post_base'); ?>
-<?php $__env->startSection('view::yields', 'post_body'); ?>
-<?php $__env->startSection('pageTitle', 'Give Your Ideas Away'); ?>
-<?php $__env->startSection('post::title', 'Give Your Ideas Away'); ?>
-<?php $__env->startSection('post::date', 'March 17, 2016'); ?>
-<?php $__env->startSection('post::brief', 'There is nothing of tangible value there yet. Could one of my contacts steal the idea? Absolutely. Could they execute it? Who knows.'); ?>
-
-            <?php $__env->startSection('post_body'); ?>
-            <p>I consult for a lot of entrepreneurs and founders. I help them out with marketing, strategy and a little coaching here and there. For the most part,
-this is a great experience, and I get to help some awesome visionaries who are excited about what they’re doing.</p>
-<p>But I get at least 2–3 emails a week from a founder who has something akin to <strong>“The Next Facebook”</strong> and wants me to sign an
-NDA before she or he can tell me about it. You know, in case I steal it.</p>
-<p><a href="https://medium.com/life-learning/give-your-ideas-away-f21d8dbf9c1">Read the complete article on Medium</a></p>
-            <?php $__env->stopSection(); ?>
-<?php echo $__env->make('_includes.blog_post_base', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
